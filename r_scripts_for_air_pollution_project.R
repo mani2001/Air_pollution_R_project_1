649f5590670f62334b7513092811e3f3bdb8c93b
mani2001 <manikantan.srinivasan2001@gmail.com>
2020-04-20 17:49:17 +0530

    complete function for Air pollution

--- r_scripts_for_air_pollution_project.R
+++ r_scripts_for_air_pollution_project.R
@@ -1,21 +1,16 @@
-#part 1 pollutant mean fundtion for both sulphate and nitrate of arbitrary ids
-pollutantmean <- function(directory, pollutant = "sulfate", id = 1:332) {
-  
-  # Create empty data frame
-  data <- data.frame(Date = as.Date(character()), sulfate=character(), nitrate=character(), ID=character())
-  # Join all the selected csv files into one dataframe
-  for (i in id) {
+#function to find complete cases for given ids
+complete<-function(directory,id=1:332)
+{
+  data_complete<-data.frame(id=numeric(),nobs=numeric())
+  for(i in id)
+  {
     filename <- sprintf("%03d.csv", i)
     filepath <- paste(directory, filename, sep="/")
     temp_data <- read.csv(filepath)
-    data <- rbind(data, temp_data)
-  } 
-  
-  
-  # Compute the mean
-  return(round(mean(data[,pollutant], na.rm = TRUE),3))
+    good<-na.omit(temp_data)
+    df<-list(id=i,nobs=nrow(good))
+    data_complete=rbind(data_complete,df)
+  }
+  data_complete
 }
-
-
-
 
