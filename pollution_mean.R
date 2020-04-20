8035bbbc8db5e5c937ba73407fffcff0b30a5466
mani2001 <manikantan.srinivasan2001@gmail.com>
2020-04-20 17:51:45 +0530

    pollution mean function

---
+++ pollution_mean.R
@@ -0,0 +1,16 @@
+pollutantmean <- function(directory, pollutant = "sulfate", id = 1:332) {
+      
+         # Create empty data frame
+        data <- data.frame(Date = as.Date(character()), sulfate=character(), nitrate=character(), ID=character())
+         # Join all the selected csv files into one dataframe
+           for (i in id) {
+                filename <- sprintf("%03d.csv", i)
+                 filepath <- paste(directory, filename, sep="/")
+                 temp_data <- read.csv(filepath)
+                 data <- rbind(data, temp_data)
+             } 
+        
+          
+           # Compute the mean
+           return(round(mean(data[,pollutant], na.rm = TRUE),3))
+     }
